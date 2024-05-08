b1f75e5791a63960ffd9df3846985d6ce903410b
Batihan Ozdemir <batihandev@gmail.com>
2024-05-08 14:09:57 +0300

    test serverlerss changes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,24 +1,31 @@
-import fastify from "fastify";
+import http from "node:http";
+import fastify, { FastifyRequest, FastifyReply } from "fastify";
 import { webhookCallback } from "grammy";
 import type { Bot } from "#root/bot/index.js";
 import { logger } from "#root/logger.js";
 
-export const createServer = async (bot: Bot) => {
+export const createServer = (bot: Bot) => {
   const server = fastify({
     logger,
+    serverFactory: (handler, _options) => {
+      const server2 = http.createServer((request, response) => {
+        (request as unknown as FastifyRequest).raw = request;
+        (response as unknown as FastifyReply).raw = response;
+        handler(request, response);
+      });
+      return server2;
+    },
   });
 
   server.setErrorHandler(async (error, request, response) => {
     logger.error(error);
-
     await response.status(500).send({ error: "Oops! Something went wrong." });
   });
 
   server.get("/", () => ({ status: true }));
-
   server.post(`/${bot.token}`, webhookCallback(bot, "fastify"));
 
   return server;
 };
 
-export type Server = Awaited<ReturnType<typeof createServer>>;
+export type Server = ReturnType<typeof createServer>;
