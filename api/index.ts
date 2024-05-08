00558e91dc78ef8e1ccc6ec5e5e3ed9a2f1f3a7f
Batihan Ozdemir <batihandev@gmail.com>
2024-05-08 14:34:01 +0300

    test3232

--- api/index.ts
+++ api/index.ts
@@ -9,5 +9,5 @@ const server = await createServer(bot);
 export default async (request: IncomingMessage, response: ServerResponse) => {
   await server.ready();
   server.server.emit("request", request, response);
-  // test
+  // test 2
 };
