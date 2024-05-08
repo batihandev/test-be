92500a74e17e5ac4a8576b2fffc1b7f10a5a2118
Batihan Ozdemir <batihandev@gmail.com>
2024-05-08 13:24:27 +0300

    testing

---
+++ api/index.ts
@@ -0,0 +1,21 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { Bot } from "#root/bot/index.js";
+import { config } from "#root/config.js";
+import { createBot } from "#root/bot/index.js";
+import { createServer } from "#root/server/index.js"; // Update this path to the actual path of your index.ts file
+
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  const bot: Bot = createBot(config.BOT_TOKEN);
+  const server = await createServer(bot);
+
+  try {
+    await server.ready();
+    server.server.emit("listening");
+  } catch (error) {
+    console.error(error);
+    response.status(500).send({ error: "Oops! Something went wrong." });
+  }
+}
