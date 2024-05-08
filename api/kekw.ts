ad31c648bf7b72bcb178e7d07a943d08d63c1f63
Batihan Ozdemir <batihandev@gmail.com>
2024-05-08 14:20:14 +0300

    testing

--- api/kekw.ts
+++ api/kekw.ts
@@ -3,7 +3,6 @@ import type { Bot } from "#root/bot/index.js";
 import { config } from "#root/config.js";
 import { createBot } from "#root/bot/index.js";
 import { createServer } from "#root/server/index.js"; // Update this path to the actual path of your index.ts file
-import { HTTPMethods } from "fastify";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const bot: Bot = createBot(config.BOT_TOKEN);
@@ -11,15 +10,9 @@ export default async (request: VercelRequest, response: VercelResponse) => {
 
   try {
     await server.ready();
-    const result = await server.inject({
-      method: request.method as HTTPMethods | undefined,
-      url: request.url,
-      headers: request.headers,
-      payload: request.body,
-    });
-    response.status(result.statusCode).send(result.body);
-  } catch (error) {
-    console.error(error);
+
+    server.server.emit("request", request, response);
+  } catch {
     response.status(500).send({ error: "Oops! Something went wrong." });
   }
 };
