53f7d4a9eaa8b171a45b9101aa7009be7e9cf4d4
Batihan Ozdemir <batihandev@gmail.com>
2024-05-08 14:21:38 +0300

    testing

--- api/index.ts
+++ api/index.ts
@@ -4,10 +4,9 @@ import { config } from "#root/config.js";
 import { createBot } from "#root/bot/index.js";
 import { createServer } from "#root/server/index.js"; // Update this path to the actual path of your index.ts file
 
+const bot: Bot = createBot(config.BOT_TOKEN);
+const server = await createServer(bot);
 export default async (request: VercelRequest, response: VercelResponse) => {
-  const bot: Bot = createBot(config.BOT_TOKEN);
-  const server = await createServer(bot);
-
   try {
     await server.ready();
 
